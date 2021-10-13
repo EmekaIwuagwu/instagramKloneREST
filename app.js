11792e1ceb18d67406c69ab134f017c3eebb0068
unknown <iwuagwuemmy@yahoo.com>
2021-10-13 08:55:47 +0100

    make it better

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var port = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+ });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended:true
